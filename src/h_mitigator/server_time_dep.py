21e0807fad8e7dc4a06ff89ba97dd35bb145f712
Paul Nikolaus <nikolaus@cs.uni-kl.de>
2022-09-29 13:40:02 +0200

    moved repository

--- src/h_mitigator/server_time_dep.py
+++
@@ -1,5 +0,0 @@
-"""Class for all service processes that cannot be described via (sigma, rho)"""
-
-
-def expect_const_rate(delta_time: int, rate: float) -> float:
-    return delta_time * rate
--- src/nc_operations/nc_analysis.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class NCAnalysis(Enum):
-    TFA = "Total Flow Analysis"
-    SFA = "Separate Flow Analysis"
-    PMOO = "Pay Multiplexing Only Once Analysis"
